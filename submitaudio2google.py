06ad58f4eaca4d7ea7e01fd94a84f9338cc35a03
Song Yu <apollos521@gmail.com>
2015-10-26 22:42:15 +0800

    simple python code to submit chinese voice

--- submitaudio2google.py
+++ submitaudio2google.py
@@ -1,1 +1,10 @@
-import request
+import requests
+import json
+
+url = "https://www.google.com/speech-api/v2/recognize"
+payload = {'output':'json', 'lang':'zh-cn', 'key':'AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'}
+headers = {'Content-Type':'audio/x-flac; rate=16000;'}
+audioFiles = {'file':open('standup.flac', 'rb')}
+r = requests.post(url, headers=headers, params=payload, files=audioFiles)
+print(r.text)
+r.json()
--- test/audio/submit2google.sh
+++ test/audio/submit2google.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 curl -X POST --data-binary @'hello (16bit PCM).wav' --header 'Content-Type: audio/l16; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'
+
+curl -X POST --data-binary @'standup.flac' --header 'Content-Type: audio/x-flac; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=zh-cn&key=AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'
+
