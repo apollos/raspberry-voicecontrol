1576c37d049e0d7581643dfd874abf0f73a28fc7
songyu <apollos521@gmail.com>
2015-10-27 17:21:19 +0800

    working

--- submitaudio2google.py
+++ submitaudio2google.py
@@ -1,10 +1,50 @@
 import requests
 import json
+import traceback 
 
+ERRORC = '###???'
 url = "https://www.google.com/speech-api/v2/recognize"
-payload = {'output':'json', 'lang':'zh-cn', 'key':'AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'}
-headers = {'Content-Type':'audio/x-flac; rate=16000;'}
-audioFiles = {'file':open('standup.flac', 'rb')}
-r = requests.post(url, headers=headers, params=payload, files=audioFiles)
-print(r.text)
-r.json()
+fullpath = "test/audio/"
+defaultLanguage = ('en-us', 'zh-ch')
+defaultRate = (44100, 16000)
+defaultApiKey = 'AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'
+defaultWaveType = ('x-flac', 'l16')
+
+def stt_google(filename, language, waveType, rate):
+    langIdx = 0
+    if not language ''
+        payload = {'output':'json', 'lang':language, 'key':defaultApiKey}
+    else
+        payload = {'output':'json', 'lang':defaultLanguage[langIdx++], 'key':defaultApiKey}
+
+    waveIdx = 0
+    if waveType ''
+        waveType = defaultWaveType[waveIdx++]
+    rateIdx = 0
+    if rate ''
+        rate = defaultRate[rateIdx++]
+
+    headers = {'Content-Type':'audio/%(wave)s; rate=%(rate)d;' % {'wave':waveType, 'rate':rate)}
+
+    if filename ''
+        print("File name error!")
+        return ERRORC
+
+    try:
+        audioFiles = {'file':open('good-morning-google.flac', 'rb')}
+    except:
+        traceback.print_exc()
+    try:
+        r = requests.post(url, headers=headers, params=payload, files=audioFiles)
+    except:
+        traceback.print_exc()
+    if r.status_code != 200
+        print("Recognize failed, %d, %s\n" % (r.status_code, r.text))
+        return ERRORC
+    result =  json.loads(r.text.split('\n',1)[1])['result'][0]['alternative'][0]
+    if result['confidence'] >= 0.5
+        return result['transcript']
+    else 
+        print("Hardly recognize. confidence is %f and possible transcript is %s\n" % (result['confidence'], result['transcript'])
+        return ERRORC
+
--- test/audio/submit2google.sh
+++ test/audio/submit2google.sh
@@ -4,3 +4,5 @@ curl -X POST --data-binary @'hello (16bit PCM).wav' --header 'Content-Type: audi
 
 curl -X POST --data-binary @'standup.flac' --header 'Content-Type: audio/x-flac; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=zh-cn&key=AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'
 
+
+curl -X POST --data-binary @'good-morning-google.flac' --header 'Content-Type: audio/x-flac; rate=44100;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyDUyyWa9Nj1aPnS_9DJsjqrrgQ6OEUHMaU'
